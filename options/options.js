dde353a813956aeb3d3a4fbe22315ae2427125ab
Crowdbotics <team@crowdbotics.com>
2022-04-08 11:04:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "myers_pizza_34654",
+  url: "https://myers_pizza_34654.botics.co",
+  api: "https://myers_pizza_34654.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
